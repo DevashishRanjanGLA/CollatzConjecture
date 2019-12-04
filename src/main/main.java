8ca0ddcf94c0f1dcf2ded84d9ae26879f3b27699
DevashishRanjanGLA <devashish.ranjan_cs18@gla.ac.in>
2019-12-05 03:41:40 +0530

    completed the main method by printing the list of collatz string.

--- src/main/main.java
+++ src/main/main.java
@@ -1,7 +1,13 @@
 package main;
 
+import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args) {
-        System.out.println("Enter ");
+    public static void Main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number you want to find collatz string of: ");
+        int number = scanner.nextInt();
+        Collatz c = new Collatz();
+        System.out.println(c.getCollatzStringForNumber(number));
     }
 }
